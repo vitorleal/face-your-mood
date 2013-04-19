13280cc3380827070b4e2a273806034a9c9147a6
Vitor Leal <vitorleal1@gmail.com>
2013-04-19 19:31:03 -0300

    added grunt

--- js/face-mood.js
+++ js/face-mood.js
@@ -1,23 +1,34 @@
 (function () {
+  //normalize getMedia
   navigator.getMedia = (navigator.getUserMedia ||
                         navigator.webkitGetUserMedia ||
                         navigator.mozGetUserMedia ||
                         navigator.msGetUserMedia);
 
+  //initial
   function faceMood() {
     var canvas = document.getElementById('canvas');
-    navigator.getMedia({ video: true }, this.showMedia, function(err) { console.log(err); });
+    return navigator.getMedia({ video: true }, this.showMedia, this.error);
   }
 
+  //show camera function
   faceMood.prototype.showMedia = function (stream) {
     var video  = document.getElementById('video'),
         button = document.querySelector('.take-photo');
 
     video.src = window.URL.createObjectURL(stream);
-    button.addEventListener('click', function () { faceMood.snapShot(video) }, false);
-  }
+    button.addEventListener('click', function () {
+      faceMood.snapShot(video);
+    }, false);
+  };
 
-  faceMood.prototype.snapShot = function (video) {
+  //Error function
+  faceMood.prototype.error = function (err) {
+    console.log(err);
+  };
+
+  //make a snapshot
+  faceMood.snapShot = function (video) {
     var w = video.videoWidth/2,
         h = video.videoHeight/2;
 
@@ -25,11 +36,7 @@
     canvas.height = h;
     canvas.getContext('2d').drawImage(video, 0, 0, w, h);
     canvas.classList.add('show');
-  }
+  };
 
-  faceMood.prototype.pause = function (video) {
-    video.pause();
-  }
-
-  camera = new faceMood();
+  var camera = new faceMood();
 })();
